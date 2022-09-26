e4cf5d88ce651756ac5924c02a3ba09752d64dc5
Robson Adorno <robsonadorno_@hotmail.com>
2022-09-25 22:09:18 -0300

    mensagem de lista vazia e implementação do método editar

--- src/app/components/client/edit/edit.component.ts
+++ src/app/components/client/edit/edit.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CustomDialogComponent } from '../../shared/custom-dialog/custom-dialog.component';
+import { Client } from '../model/client';
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  @Input() client?: Client;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {}
 
   onClick(): void {
-    alert('Edit');
+    this.openDialog();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(CustomDialogComponent, {
+      width: '550px',
+      data: {name: this.client?.name,
+         cpf: this.client?.cpf, age: this.client?.age},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      
+    });
   }
 }
